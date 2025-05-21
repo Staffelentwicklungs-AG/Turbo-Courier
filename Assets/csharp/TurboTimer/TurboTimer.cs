2d44034d3d5bbc2706e01b2067c8bed28becb33e
Maria Lindling <maria.neith.lindling@protonmail.com>
2025-05-21 15:54:59 +0200

    Added initial shell for the "TurboTimer"

---
+++ Assets/csharp/TurboTimer/TurboTimer.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class TurboTimer : MonoBehaviour
+{
+    #region Unity Engine Exposed Fields
+    /// <summary>
+    /// Time is given in miliseconds.
+    /// </summary>
+    [SerializeField] private int startTime;
+    #endregion
+
+    #region MonoBehavior Methods
+    /// <summary>
+    /// Start is called once before the first execution of Update after the MonoBehaviour is created
+    /// </summary>
+    void Start()
+    {
+        
+    }
+
+    /// <summary>
+    /// Update is called once per frame
+    /// </summary>
+    void Update()
+    {
+        //
+    }
+    #endregion
+}
